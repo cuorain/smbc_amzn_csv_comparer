08a3b3970a928a0c24ec1bd457410baa110555c0
cuorain <cuore623.prog@gmail.com>
2022-05-24 20:15:10 +0900

    微修正

--- src/test/java/com/example/demo/AmazonCsvReaderTests.java
+++ src/test/java/com/example/demo/AmazonCsvReaderTests.java
@@ -18,8 +18,7 @@ public class AmazonCsvReaderTests {
 			String fileName = AmazonCsvReaderTests.class.getClassLoader().getResource("Amazon_Test.csv").getPath();
 			FileInputStream inputFile = new FileInputStream(fileName);
 			MockMultipartFile file = new MockMultipartFile("testFile", inputFile);
-			final AmazonCsvReader reader = new AmazonCsvReader();
-			List<String[]> readData = reader.read(file);
+			List<String[]> readData = AmazonCsvReader.read(file);
 			assertThat(readData.get(0)).isEqualTo(new String[] {"2022/5/11","達人に学ぶDB設計 徹底指南書 初級者で終わりたくないあなたへ\r\n達人に学ぶ SQL徹底指南書 (CodeZine BOOKS)","0"});
 			assertThat(readData.get(1)).isEqualTo(new String[] {"2022/5/9","テスト駆動開発","2429"});
 			assertThat(readData.get(2)).isEqualTo(new String[] {"2022/4/30","マウントハーゲン オーガニック フェアトレード カフェインレスインスタントコーヒー100g インスタント","1491"});
